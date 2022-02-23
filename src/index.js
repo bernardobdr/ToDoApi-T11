5023ef486c9efbd3d3c39771daed485a69527fa1
cinmcantu <cinthiamcantu@gmail.com>
2022-02-22 21:31:12 -0300

    criando rota get /tarefa

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get('/usuario', (req, res)=>{
     res.send('Rota GET para entidade usuário')
 })
 
+app.get('/tarefa', (req, res)=>{
+    res.send('Rota GET para entidade tarefa')
+})
+
 
 app.listen(port, ()=>{
     console.log(`Servidor aberto na http://localhost:${port}/`)
