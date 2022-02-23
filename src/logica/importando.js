ff104b86f1a1d910f9f4c378822da946b4d88bf2
cinmcantu <cinthiamcantu@gmail.com>
2022-02-22 21:10:53 -0300

    Trabalhando com import e export

---
+++ src/logica/importando.js
@@ -0,0 +1,9 @@
+// import {nome, objeto, soma} from './exportando.js' //-> Destructuring
+import coisas from './exportando.js'
+
+console.log(coisas.nome)
+console.log(coisas.objeto)
+console.log(coisas.soma(1,2))
+// console.log(soma(1,2))
+// console.log(nome)
+// console.log(objeto)
