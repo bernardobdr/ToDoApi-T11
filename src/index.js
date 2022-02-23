9472286297ae869edcf41cb4d0ca27089654815b
cinmcantu <cinthiamcantu@gmail.com>
2022-02-22 21:11:53 -0300

    Organizando o projeto no padrao MVC

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// Importando o framework
 import express from "express";
 
+// importanto os controllers
+import usuarioController from "./controllers/usuario-controller.js";
+import tarefaController from "./controllers/tarefa-controller.js";
+
+// Instanciando/criando servidor
 const app = express()
+// Escolhendo a porta
 const port = 3000
 
-app.get('/usuario', (req, res)=>{
-    res.send('Rota GET para entidade usuário')
-})
+// chamando os controllers
+usuarioController(app)
+tarefaController(app)
 
-app.get('/tarefa', (req, res)=>{
-    res.send('Rota GET para entidade tarefa')
-})
-
-
+// Abrindo o servidor na porta escolhida
 app.listen(port, ()=>{
     console.log(`Servidor aberto na http://localhost:${port}/`)
 })
