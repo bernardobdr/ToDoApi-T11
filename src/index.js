48f2287a33c1d6b6d8cd8956a91bccfb47bfe838
cinmcantu <cinthiamcantu@gmail.com>
2022-02-24 23:07:15 -0300

    Introducao de middlewares e utilizacao de parser no req.body

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@ import express from "express";
 import usuarioController from "./controllers/usuario-controller.js";
 import tarefaController from "./controllers/tarefa-controller.js";
 
+// importando os middlewares
+import generalMiddleware from "./middleware/general-middleware.js";
+import usuarioMiddleware from "./middleware/usuario-middleware.js";
+import tarefaMiddleware from "./middleware/tarefa-middleware.js";
+
 // Instanciando/criando servidor
 const app = express()
 // Escolhendo a porta
 const port = 3000
+
+// Middleware necessario para fazer o parser do 
+// JSON recebido do body em objeto
+app.use(express.json())
+
+// Chamada dos Middlewares especificos das rotas
+generalMiddleware(app) // vai ser rodados em todas as rotas
+usuarioMiddleware(app) // vai ser rodado só para rotas de usuario
+tarefaMiddleware(app)  // vai ser rodado só para rotas de tarefa
 
 // chamando os controllers
 usuarioController(app)
