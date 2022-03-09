dd8699e67043fcf06d2b617c1df97fa2d9aa8b33
cinmcantu <cinthiamcantu@gmail.com>
2022-03-09 20:48:55 -0300

    Inserindo parametros e criando rotas delete e put

--- src/controllers/tarefa-controller.js
+++ src/controllers/tarefa-controller.js
@@ -12,6 +12,20 @@ const tarefaController = (app, bd)=>{
         })
     })
 
+    app.get('/tarefa/titulo/:titulo', (req, res)=>{
+        // Pegando parametro que sera utilizado para o filtro
+        const titulo = req.params.titulo
+
+        // Pesquisa a tarefa no banco de dados
+        const tarefaEncontrada = bd.tarefas.filter(tarefa=>(tarefa.titulo == titulo))
+
+        // Retorna a tarefa encontrada
+        res.json({
+            "tarefa": tarefaEncontrada,
+            "erro": false
+        })
+    })
+
     app.post('/tarefa',(req, res)=>{
         // Recebe o corpo da requisição
         const body = req.body
@@ -32,6 +46,57 @@ const tarefaController = (app, bd)=>{
                 "tarefas": novaTarefa,
                 "erro": false
             })
+        } catch (error) {
+            // Envia o erro, caso exista
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
+        }
+    })
+
+    app.delete('/tarefa/titulo/:titulo', (req, res)=>{
+        // Pegando parametro que sera utilizado para o filtro
+        const titulo = req.params.titulo
+
+        // remove a tarefa do banco de dados
+        const novoDB = bd.tarefas.filter(tarefa=>(tarefa.titulo !== titulo))
+        bd.tarefas = novoDB
+
+        // Resposta com o retorno
+        res.json({
+            "msg": `Tarefa de título ${titulo} excluida com sucesso`,
+            "erro": false
+        })
+    })
+
+    app.put('/tarefa/titulo/:titulo', (req, res)=>{
+        // Pegando parametro que sera utilizado para o filtro
+        const titulo = req.params.titulo
+
+        // Pegando o corpo da requisição com as informações
+        // que serão atualizados
+        const body = req.body
+
+        try {
+            // utiliza a classe para validação dos dados recebidos
+            const tarefaAtualizada = new Tarefa(body.titulo, body.descricao, body.status)
+
+            // Atualiza a tarefa no banco de dados
+            bd.tarefas = bd.tarefas.map(tarefa => {
+                if(tarefa.titulo === titulo){
+                    return tarefaAtualizada
+                }
+                return tarefa    
+            });
+
+            // Resposta com o retorno
+            res.json({
+                "msg": `Tarefa de titulo ${tarefaAtualizada.titulo} atualizada com sucesso`,
+                "tarefa": tarefaAtualizada,
+                "erro": false
+            })
+
         } catch (error) {
             // Envia o erro, caso exista
             res.json({
